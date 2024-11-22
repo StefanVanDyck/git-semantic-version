74f87fd794dc30d14b9a8c0aed79accff9275509
StefanVanDyck <vandyck.stefan@gmail.com>
2024-11-22 20:15:35 +0100

    Fix output of action (#5)
    Fixed issue with the job outputs

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,4 @@ FROM bitnami/git:2
 COPY version.sh /version.sh
 
 ENTRYPOINT [ "bash", "-c" ]
-CMD ["/version.sh", ">>", "$GITHUB_OUTPUT"]
+CMD ["/version.sh >> $GITHUB_OUTPUT"]
--- version.sh
+++ version.sh
@@ -46,7 +46,7 @@ fi
 _LAST_VERSION_TAG="$(git describe --tags --abbrev=0 --match="${_COMPONENT_VERSION_PREFIX}[0-9]*.[0-9]*.[0-9]*" || true)"
 if [ -z "${_LAST_VERSION_TAG}" ]; then
   echo >&2 "No tags found defaulting to 0.0.1"
-  echo "number_of_changes_since_last_tag=-1"
+  echo "number_of_changes_since_last_tag="
   echo "previous_version="
   echo "new_version=0.0.1"
   exit 0
